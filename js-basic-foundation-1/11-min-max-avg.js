a26b65846cba89ed880f32109ea14fd917cf5048
IURII LYTVYN <lytvyn.139@gmail.com>
2020-05-26 17:22:38 -0500

    min max avg forEach

--- js-basic-foundation-1/11-min-max-avg.js
+++ js-basic-foundation-1/11-min-max-avg.js
@@ -1,27 +1,20 @@
-//:wq  Max/Min/Avg - Given an array with multiple values, write a function that returns a new array that only contains the 
+//Max/Min/Avg - Given an array with multiple values, write a function that returns a new array that only contains the 
 ///maximum, minimum, and average values of the original array. (e.g. [1,5,10,-2] will return [10,-2,3.5]) 
 const arr = [1, 5, 10, -2];
 
-maxMinAvg = (arr) => {
-  const result = [];
-  const max = arr[0];
-  const min = arr[0];
-  let avg = 0;
-  //maximum
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-      result[0] = arr[i];
-    }
-    //minimum
-    if (arr[i] < min) {
-      result[1] = arr[i];
-    }
-    //avg
-    avg += arr[i] / arr.length;
-    result[2] = avg;
-  }
-  return result;
+function maxMinAvg(arr) {
+    let max = arr[0];
+    let min = arr[0];
+    let sum = 0;
+    arr.forEach(function(value) {
+        if (value > max)
+            max = value;
+        if (value < min)
+            min = value;
+        sum += value;
+    })
+    let avg = sum / arr.length;
+    return [max, min, avg];
 }
-
 let final = maxMinAvg(arr);
 console.log(final);
