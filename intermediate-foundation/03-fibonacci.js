b04dc15727c3701bad1ec96d286354aea69b0da9
IURII LYTVYN <lytvyn.139@gmail.com>
2020-05-27 22:09:07 -0500

    added another variant

--- intermediate-foundation/03-fibonacci.js
+++ intermediate-foundation/03-fibonacci.js
@@ -12,36 +12,69 @@ Your function should accept one argument, an index into the sequence (where 0 co
  fibonacci(7) = 13 (5+8).  
  Do this without using recursion first.  
  If you don't know what a recursion is yet, don't worry as we'll be introducing this concept in Part 2 of this assignment. */
-
 //F(n) = F(n-1) + F(n-2)
 //100%
-fibonacci = (number) => {
+
+//https://jsben.ch/WD8tp
+
+//#1
+fibonacciForLoop = (number) => {
   let previous_first = 0,
     previous_second = 1,
     next = 1;
-
   for (let i = 2; i <= number; i++) {
     next = previous_first + previous_second;
     previous_first = previous_second;
     previous_second = next;
   }
   return next;
+};
+console.log(fibonacciForLoop(20));
+
+//#2
+function fibonacciWhileLoop(num) {
+  let a = 1;
+  let b = 0;
+  let temp = 0;
+
+  while (num >= 0) {
+    temp = a;
+    a = a + b;
+    b = temp;
+    num--;
+  }
+  return b;
 }
-console.log(fibonacci(7));
+fibonacciWhileLoop(20);
 
-// //0.68% slow on tests !!!
-// function fiboLoop(num) {
-//   let a = 1;
-//   let b = 0;
-//   let temp = 0;
+/* 
+This implementation is concise and easy to understand.
+We just need have base case when n <=2 and do recursive calls on n-1 & n-2.
+The drawback is 1 call becomes 2 calls. 2 calls becomes 4. etc. It is exponential.
+Time complexity O(2n) and space complexity is also O(2n) for all stack calls.
+//Fibonacci algorithm using recursion time complexity.
+//F(n) = F(n-1)+F(n-2)
+*/
+//#3
+fibonacciRecursion = (n) => {
+  if (n <= 1) {
+    return n;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+};
+console.log(fibonacciRecursion(20));
 
-//   while (num >= 0) {
-//     temp = a;
-//     a = a + b;
-//     b = temp;
-//     num--;
-//     console.log('num: ' + num);
-//   }
-//   return b;
-// }
-// fiboLoop(10);
+//#4
+fibBottomUp = (n) => {
+  let mem = [];
+  for (let i = 0; i <= n; i++) {
+    if (i == 0) mem[i] = 0;
+    else if (i <= 2) mem[i] = 1;
+    else {
+      mem[i] = mem[i - 1] + mem[i - 2];
+    }
+  }
+  return mem[n];
+}
+fibBottomUp(20);
--- js-basic-foundation-2/06-even-and-oddds.js
+++ js-basic-foundation-2/06-even-and-oddds.js
@@ -3,8 +3,8 @@ Create a function that accepts an array.
 Every time that array has three odd values in a row, print "That's odd!".  
 Every time the array has three evens in a row, print "Even more so!".*/
 
+// my code
 const arr = [1, 3, 7, 10, 6, 8, 10];
-
 evensAndOdds = () => {
   let countOdd = 0;
   let countEven = 0;
@@ -16,15 +16,29 @@ evensAndOdds = () => {
       countOdd++;
       if (i !== 0 && countEven !== 0) countEven = 0;
     }
-
     if (countOdd === 3) {
       console.log("That's odd!");
       countOdd = 0;
-    } else if (countEven == 3) {
+    } else if (countEven === 3) {
       console.log("Even more so!");
       countEven = 0;
     }
   }
-
-}
+};
 let z = evensAndOdds(arr);
+
+// teachers code :'(
+// const arr = [1, 3, 7, 10, 6, 8, 10];
+// evensAndOdds = () => {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 !== 0 && arr[i + 1] % 2 !== 0 && arr[i + 2] % 2 !== 0) {
+//       console.log('Odd');
+//       i += 2;
+//     } else if (arr[i] % 2 === 0 && arr[i + 1] % 2 === 0 && arr[i + 2] % 2 === 0) {
+//       console.log('Even more so');
+//       i += 2;
+
+//     }
+//   }
+// }
+// evensAndOdds(arr);
